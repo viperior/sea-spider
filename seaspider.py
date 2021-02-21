a89a06ac0c5f12d1c364b2a0d5f1406ef01f276d
viperior <viperior@gmail.com>
2021-02-21 13:19:12 -0800

    Move primary logic into crawl_from_origin function

--- seaspider.py
+++ seaspider.py
@@ -12,6 +12,32 @@ def check_url(url):
     print('\n', url_check_result['status_code'], ' ', url)
     return url_check_result
 
+def crawl_from_origin(origin_url, domain_restriction=''):
+    """Crawl a given starting URL, collect all links from its HTML, and then
+    recursively crawl those links, while avoiding duplicate crawls."""
+    crawl_queue = {}
+    crawl_result = crawl_target(origin_url)
+    print(crawl_result['status_code'], ' ', origin_url, ' ORIGIN')
+    soup = bs4.BeautifulSoup(crawl_result['text'], features='html.parser')
+    pattern = '^https?://' + domain_restriction
+    links = soup.findAll('a', attrs={'href': re.compile(pattern)})
+    print(len(links), ' links detected')
+
+    for link in links:
+        url = link.get('href')
+        
+        if not url in crawl_queue.keys():
+            crawl_queue[url] = {}
+
+    for key in crawl_queue.keys():
+        print(key)
+
+    progress_bar_label = 'Crawling ' + str(len(crawl_queue)) + ' URLs'
+
+    for key in tqdm(crawl_queue.keys(), desc=progress_bar_label):
+        crawl_queue[key]['crawl_result'] = check_url(key)
+        time.sleep(0.1)
+
 def crawl_target(target_url):
     crawl_result = {}
     r = requests.get(target_url, headers={'User-Agent': 'Sea'})
@@ -20,39 +46,22 @@ def crawl_target(target_url):
     return crawl_result
 
 def main():
-    """Crawl a given starting URL, collect all links from its HTML, and then
-    recursively crawl those links, while avoiding duplicate crawls."""
-    crawl_queue = {}
-
     if len(sys.argv) < 2:
         print('[ERROR] No target URL supplied. Please provide a URL for seaspider to crawl.')
     else:
-        target_url = sys.argv[1]
-        crawl_result = crawl_target(target_url)
-        print(crawl_result['status_code'], ' ', target_url)
-        soup = bs4.BeautifulSoup(crawl_result['text'], features='html.parser')
-        pattern = '^https?://'
-
-        # Apply domain restriction
+        origin_url = sys.argv[1]
+        
         if len(sys.argv) >= 3:
-            pattern += sys.argv[2]
-
-        links = soup.findAll('a', attrs={'href': re.compile(pattern)})
-        print(len(links), ' links detected')
-
-        for link in links:
-            url = link.get('href')
+            domain_restriction = sys.argv[2]
+            crawl_from_origin(origin_url, domain_restriction)
+        else:
+            domain_restriction_warning = 'You are about to crawl with domain'\
+                'restriction. Are you sure? (y/n) '
+            user_input = input(domain_restriction_warning)
             
-            if not url in crawl_queue.keys():
-                crawl_queue[url] = {}
-
-        for key in crawl_queue.keys():
-            print(key)
-
-        progress_bar_label = 'Crawling ' + str(len(crawl_queue)) + ' URLs'
-
-        for key in tqdm(crawl_queue.keys(), desc=progress_bar_label):
-            crawl_queue[key]['crawl_result'] = check_url(key)
-            time.sleep(0.1)
+            if user_input == 'y':
+                crawl_from_origin(origin_url)
+                
+    print('Ending session...')
             
 main()
